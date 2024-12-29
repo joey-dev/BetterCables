12c87405be006ce6c3ba341d981fc376857ee301
joey-dev <joeystil3@gmail.com>
2024-12-29 20:34:37 +0100

    baseline

---
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -0,0 +1,25 @@
+package com.example.examplemod;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
+public class ExampleMod
+{
+    public static final String MODID = "examplemod";
+    public static final String NAME = "Example Mod";
+    public static final String VERSION = "1.0";
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        // Empty for testing
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        // Empty for testing
+    }
+}
