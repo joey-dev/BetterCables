4137eed5cb7e30be81fdde060e990a9d7babbfa1
joey-dev <joeystil3@gmail.com>
2025-01-08 20:43:05 +0100

    [wip] First custom model of a block

--- src/main/java/com/emorn/bettercables/init/BlockInit.java
+++ src/main/java/com/emorn/bettercables/init/BlockInit.java
@@ -1,13 +1,16 @@
 package com.emorn.bettercables.init;
 
 import com.emorn.bettercables.objects.blocks.BlockBase;
+import com.emorn.bettercables.objects.blocks.BlockSantaHat;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 
 import java.util.*;
 
-public class BlockInit {
+public class BlockInit
+{
     public static final List<Block> BLOCKS = new ArrayList<Block>();
 
     public static final Block ORE_COPPER = new BlockBase("copper_ore", Material.IRON);
+    public static final Block SANTA_HAT = new BlockSantaHat("santa_hat");
 }
