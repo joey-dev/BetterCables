4a679c3d2d8962289e08e6ccb1a7e43c139d1a7c
Joey <joeystil3@gmail.com>
2025-01-20 19:37:13 +0100

    Able to insert and extract from inventories (#11)
    Implementation of an inventory management system for Better Cables mod that enables item transfers between connected chests through a connector network. The system features inventory position management with protective bounds checking, safe item extraction/insertion functionality, and a tick-based transfer regulation system with error handling to prevent array index exceptions during storage operations

--- src/main/java/com/emorn/bettercables/objects/blocks/connector/ConnectorNetwork.java
+++ src/main/java/com/emorn/bettercables/objects/blocks/connector/ConnectorNetwork.java
@@ -5,10 +5,7 @@ import net.minecraft.util.math.BlockPos;
 
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @MethodsReturnNonnullByDefault
 @ParametersAreNonnullByDefault
@@ -35,6 +32,38 @@ public class ConnectorNetwork
         }
 
         id = savedId;
+    }
+
+    public int findNextIndex(int index) {
+        this.cleanInsertInventoryPositions();
+
+        int totalItems = this.insertInventoryPositions.size();
+        index++;
+
+        if (index >= totalItems) {
+            index = 0;
+        }
+
+        return index;
+    }
+
+    @Nullable
+    public BlockPos findInventoryPositionBy(int index)
+    {
+        this.cleanInsertInventoryPositions();
+
+        int totalItems = this.insertInventoryPositions.size();
+
+        if (index >= totalItems) {
+            return null;
+        }
+
+        return (BlockPos) this.insertInventoryPositions.keySet().toArray()[index];
+    }
+
+    private void cleanInsertInventoryPositions() // todo this is prob a bug
+    {
+        this.insertInventoryPositions.entrySet().removeIf(entry -> entry.getValue().isEmpty());
     }
 
     public static ConnectorNetwork create(int savedId)
